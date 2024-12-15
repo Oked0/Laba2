0d3a62081b582059164ce339d3ecf440ce4a14ab
Oked0 <daniilchertov44@gmail.com>
2024-12-15 16:07:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/laba2ispravl/FourthActivity.java
@@ -0,0 +1,25 @@
+package com.example.laba2ispravl;
+
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FourthActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fourth);
+
+        Button customButton = findViewById(R.id.custom_button);
+        GradientDrawable border = new GradientDrawable();
+        border.setCornerRadius(24);
+        border.setStroke(12, 0xFF505050);
+        border.setColor(0xFFFFFFFF);
+        customButton.setBackground(border);
+
+        customButton.setOnClickListener(v -> {
+            border.setColor(0xFF90EE90);
+        });
+    }
+}
